f4b1dc8cd0a4738a4e5ba33c10d885ebe312a96e
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2019-04-15 16:48:51 +0100

    Initial check-in of module AspCoreCRUD

---
+++ AspCoreCRUD/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace AspCoreCRUD.Models
+{
+    public class Employee
+    {
+        public int id
+        {
+            get;
+            set;
+        }
+
+        public String firstName
+        {
+            get;
+            set;
+            }
+
+        public String lastName
+        {
+            get;
+            set;
+        }
+
+        public Employee()
+        {
+
+        }
+
+    }
+}
